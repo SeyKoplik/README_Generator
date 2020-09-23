c7c920de716e3b34dd2e1e6785b8340c933468e0
Seychelle Koplik <sey.koplik@gmail.com>
2020-09-22 21:26:27 -0400

    added questions object to use for inquirer.prompt function

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,68 @@
+// var inquirer = require("inquirer");
+// var fs = require('fs');
+
+const inquirer = require("inquirer");
+
 // array of questions for user
 const questions = [
-
+    {
+    type: "input",
+    name: "Questions",
+    message: "Please enter your GitHub username: "
+    },
+    {
+    type: "input",
+    name: "emailAddress",
+    message: "Please enter your email address: "
+    },
+    {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?"
+    },
+    {
+    type: "input",
+    name: "description",
+    message: "What is your project's description?"
+    },
+    {
+    type: "input",
+    name: "instruction",
+    message: "Any installation instructions?"
+    },
+    {    
+    type: "list",
+    message: "What type of license do you require?",
+    name: "contact",
+    choices: [
+      "MIT",
+      "Apache 2.0",
+      "GPL 3.0",
+      "BSD 3",
+      "None" ]
+    },
+    {
+    type: "input",
+    name: "usage",
+    message: "Do you have any usage directions to add?"
+    },
+    {
+    type: "input",
+    name: "contribution",
+    message: "Are there any contributions you would like to add?"
+    },
+    {
+    type: "input",
+    name: "test",
+    message: "What kind of tests??"
+    },
 ];
+
+inquirer.prompt(questions);
 
 // function to write README file
 function writeToFile(fileName, data) {
+
 }
 
 // function to initialize program
@@ -17,15 +75,9 @@ init();
 
 
 //===================//
-// var inquirer = require("inquirer");
-// var fs = require('fs');
 
 // inquirer.prompt([
-//   {
-//     type: "input",
-//     name: "name",
-//     message: "What is your name?"
-//   },
+
 //   {
 //     type: "checkbox",
 //     message: "What languages do you know?",
